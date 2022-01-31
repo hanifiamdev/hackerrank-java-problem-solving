82941884908bfde8dc231a200d8639a1e0697dda
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-31 07:02:56 +0700

    Simple Array Sum

--- src/com/hanifiamdev/Solution.java
+++ src/com/hanifiamdev/Solution.java
@@ -1,9 +1,44 @@
 package com.hanifiamdev;
 
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
 
 public class Solution {
-    public static void main(String[] args) {
 
+    public static int simpleArraySum(List<Integer> ar) {
+        // Write your code here
+        int sum = 0;
+        for (Integer item : ar) {
+            sum += item;
+        }
+        return sum;
     }
 
+    /* Test :
+     * 6
+     * 1 2 3 4 10 11
+     * 31
+     * */
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
