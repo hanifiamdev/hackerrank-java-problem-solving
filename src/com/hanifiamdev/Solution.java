9060540ba151bd7bf928a05b8198b56165f82ecc
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-31 07:57:59 +0700

    A Very Big Sum

--- src/com/hanifiamdev/Solution.java
+++ src/com/hanifiamdev/Solution.java
@@ -1,9 +1,55 @@
 package com.hanifiamdev;
 
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
 
-public class Solution {
-    public static void main(String[] args) {
+import static java.util.stream.Collectors.toList;
 
+class Result {
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     *
+     * Test :
+     * 5
+     *1000000001  1000000002 1000000003 1000000004 1000000005
+     *5000000015
+     */
+
+    public static long aVeryBigSum(List<Long> ar) {
+        // Write your code here
+        Long result = 0L;
+        for (Long aLong : ar) {
+            result = result + aLong;
+        }
+        return result;
     }
 
 }
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long result = Result.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
