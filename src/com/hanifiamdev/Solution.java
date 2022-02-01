0864b9f6646b85da345abfe750b2df68aa86383c
hanifiamdev <hanifamrullahdev@gmail.com>
2022-02-01 08:15:53 +0700

    Sum nilai dari elemen array dengan takeout out salah satu nilai ,kemudian cari hasil in dan max dari sum

--- src/com/hanifiamdev/Solution.java
+++ src/com/hanifiamdev/Solution.java
@@ -1,9 +1,57 @@
 package com.hanifiamdev;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
 
-public class Solution {
-    public static void main(String[] args) {
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     *
+     * Test :
+     * 1 2 3 4 5
+     * 10 14
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        long min = 0, max = 0, sum = 0;
+        min = arr.get(0);
+        max = min;
+        sum = min;
+        for (int i = 1; i < arr.size(); i++) {
+            sum += arr.get(i);
+            if (arr.get(i) < min) {
+                min = arr.get(i);
+            }
+            if (arr.get(i) > max) {
+                max = arr.get(i);
+            }
+        }
+        System.out.format("%d %d", sum - max, sum - min);
 
     }
 
 }
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
+
