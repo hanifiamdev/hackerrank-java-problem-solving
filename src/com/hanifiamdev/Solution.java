b573070d32e442324c87d6d393651241cb383496
hanifiamdev <hanifamrullahdev@gmail.com>
2022-02-03 10:09:10 +0700

    kangaroo number line jumps

--- src/com/hanifiamdev/Solution.java
+++ src/com/hanifiamdev/Solution.java
@@ -1,9 +1,63 @@
 package com.hanifiamdev;
 
+import java.io.*;
 
-public class Solution {
-    public static void main(String[] args) {
+class Result {
+
+    /*
+     * Complete the 'kangaroo' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x1
+     *  2. INTEGER v1
+     *  3. INTEGER x2
+     *  4. INTEGER v2
+     *
+     * Test 1 :
+     * 0 2 5 3
+     ^ NO
+     *
+     * Test 2 :
+     * 0 3 4 2
+     ^ YES
+     */
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        // Write your code here
+        int vdiff = v1 - v2;
+        if (vdiff <= 0) {
+            return "NO";
+        }
+        int xdiff = x1 - x2;
+        return (xdiff % vdiff == 0 ? "YES" : "NO");
 
     }
 
 }
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int x1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int v1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int x2 = Integer.parseInt(firstMultipleInput[2]);
+
+        int v2 = Integer.parseInt(firstMultipleInput[3]);
+
+        String result = Result.kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
