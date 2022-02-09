97abb0d29464a83761c1618c8f732ba4d6c9267e
hanifiamdev <hanifamrullahdev@gmail.com>
2022-02-09 11:59:36 +0700

    Count Min Max

--- src/com/hanifiamdev/Solution.java
+++ src/com/hanifiamdev/Solution.java
@@ -1,9 +1,71 @@
 package com.hanifiamdev;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
-public class Solution {
-    public static void main(String[] args) {
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
+class Result {
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        // Write your code here
+        int countMin = 0;
+        int countMax = 0;
+        Integer highestScore = null;
+        Integer lowestScore = null;
+        for (Integer score : scores) {
+            if (highestScore != null && lowestScore != null) {
+                if (score > highestScore) {
+                    highestScore = score;
+                    countMax++;
+                }
+                if (score < lowestScore) {
+                    lowestScore = score;
+                    countMin++;
+                }
+
+            } else {
+                highestScore = score;
+                lowestScore = score;
+            }
+
+        }
+        return Arrays.asList(countMax, countMin);
     }
 
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> scores = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
